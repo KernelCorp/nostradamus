14460f2acbf43b3b569e6075369cc761f5ee122f
Zamyatin-AA <stekbox@gmail.com>
2014-04-24 17:05:01 +0600

    vk_omniauth nearly done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:vkontakte]
 
   ## Database authenticatable
   field :email,              type: String, default: ""
@@ -22,6 +23,20 @@ class User
   field :last_sign_in_at,    type: Time
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
+
+  field :provider, type: String
+  field :url, type: String
+  field :username, type: String
+  field :nickname, type: String
+  field :email, type: String
+
+  def self.find_for_vkontakte_oauth access_token
+    if user = User.where(:url => access_token.info.urls.Vkontakte).first
+      user
+    else
+      User.create!(:provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => SecureRandom.hex(6) + '@gmail.com', :password => Devise.friendly_token[0,20])
+    end
+  end
 
   ## Confirmable
   # field :confirmation_token,   type: String
