017d2f933c3cdf12f716ca087f3c8dbd43f3cc55
Zamyatin-AA <stekbox@gmail.com>
2014-04-24 17:10:42 +0600

    vk_omniauth final

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -254,6 +254,6 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :vkontakte, '4378476', '7hbQh8EqBqVdHRmfVida'
+  config.omniauth :vkontakte, '4378495', 'eyvQ9rbVRsaacfAoR5ci'
 
 end
