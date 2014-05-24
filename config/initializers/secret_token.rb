ea1bbfa9a70bd251cb92c40cd1c44ccfe55f408f
max <mk@kernel-corp.com>
2014-05-24 13:28:01 +0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nostradamus::Application.config.secret_key_base = 'd2c1ffa281ce891a4777c099015fbff402430b7cdca8502ef029a18726eb6e96ade2130aab39ed4fca59eeee8826d879648424b27db1ba7b9a9f0967c2f6ff10'
