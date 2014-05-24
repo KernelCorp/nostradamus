2ecea167a4397ce2eb3bc65555de0ec642dac269
Savelyev-Aleksey <saleksey92@gmail.com>
2014-05-24 15:29:26 +0700

    change app layout and added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Nostradamus::Application.routes.draw do
   resources :categories
 
   devise_for :users
+
+  root 'categories#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
