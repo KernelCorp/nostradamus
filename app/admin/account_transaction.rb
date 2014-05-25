0af427f87f9132a27f738b1901bd701996a08f9c
max <mk@kernel-corp.com>
2014-05-25 13:38:00 +0700

    execute transaction from AA

--- app/admin/account_transaction.rb
+++ app/admin/account_transaction.rb
@@ -1,7 +1,26 @@
 ActiveAdmin.register AccountTransaction do
 
-  controller do
-    defaults finder: :find
+  permit_params :user, :question, :status, :value, :action
+
+  # controller do
+  #   defaults finder: :find
+  # end
+
+  member_action :execute do
+    transaction = AccountTransaction.find params[:id]
+    transaction.execute_transaction
+    redirect_to action: :index
   end
-  
+
+  index do
+    selectable_column
+    column :id
+    column :status
+    column :value
+    column do |tr|
+      link_to 'execute', execute_admin_account_transaction_path(tr)
+    end
+    actions
+  end
+
 end
