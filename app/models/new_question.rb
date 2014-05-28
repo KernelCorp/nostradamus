498af6196089853bc8d1de799ae0b8f6b31bfd47
max <mk@kernel-corp.com>
2014-05-28 19:36:51 +0700

    1 step for migrate question

---
+++ app/models/new_question.rb
@@ -0,0 +1,40 @@
+class NewQuestion
+  include Mongoid::Document
+  include Mongoid::Paperclip
+
+  field :title, type: String
+  field :text, type: String
+  field :end_date, type: Time
+  field :start_date, type: Time
+  field :right_answer, type: Mongoid::Boolean
+  field :status, type: String
+
+  belongs_to :category
+
+  has_many :answers
+  has_many :account_transactions
+
+  has_mongoid_attached_file :image
+
+  validates_attachment_content_type :image, content_type: %w(image/jpg image/jpeg image/png)
+
+  def yield_outcome
+    winning_answers = self.answers.where(value: self.right_answer)
+    if winning_answers.count > 0
+      winning_value = (self.answers.count * 10 / winning_answers.count).to_i
+      winning_answers.each do |answer|
+        winning_user = answer.user
+        transaction = AccountTransaction.create!(value: winning_value, user: winning_user, status: 'pending', action: 'win')
+        transaction.execute!
+      end
+    end
+  end
+
+  def active?; !(self.status == 'closed') end
+
+  def community_guessed?
+    return false if self.answers.count == 0
+    (self.answers.where(value: self.right_answer).count * 100 / self.answers.count) >= 50
+  end
+
+end
--- lib/tasks/nostradamus.rake
+++ lib/tasks/nostradamus.rake
@@ -2,4 +2,13 @@ namespace :nostradamus do
   task category_resave: :environment do
     Category.each &:save
   end
+
+  task migrate_question: :environment do
+    Category.each do |cat|
+      cat.questions.each do |q|
+        NewQuestion.create _id: q.id, title: q.title, text: q.text, end_date: q.end_date, start_date: q.start_date,
+                           right_answer: q.right_answer, status: q.status, category: cat, image: q.image
+      end
+    end
+  end
 end
