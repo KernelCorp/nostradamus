60d53b138c33c71cf8d290236f8a0e5e0b2f079a
max <mk@kernel-corp.com>
2014-06-16 12:00:13 +0700

    fix date time

--- app/controllers/concerns/date_time_params.rb
+++ app/controllers/concerns/date_time_params.rb
@@ -4,11 +4,11 @@ module DateTimeParams
   included do
     protected 
       def set_datetime(resource, field)
-        params[resource][field] = DateTime.new params[resource].delete("#{field}(1i)").to_i,
-                                               params[resource].delete("#{field}(2i)").to_i,
-                                               params[resource].delete("#{field}(3i)").to_i,
-                                               params[resource].delete("#{field}(4i)").to_i,
-                                               params[resource].delete("#{field}(5i)").to_i
+        params[resource][field] = Time.zone.local params[resource].delete("#{field}(1i)").to_i,
+                                                  params[resource].delete("#{field}(2i)").to_i,
+                                                  params[resource].delete("#{field}(3i)").to_i,
+                                                  params[resource].delete("#{field}(4i)").to_i,
+                                                  params[resource].delete("#{field}(5i)").to_i
       rescue
       end
   end
