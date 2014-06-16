02af33154e508728b598bd77ca984da525583d7c
Dmitry Maganov <vonagam@gmail.com>
2014-06-16 15:12:53 +0700

    add vk social button

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  def vk_button( title, description, html )
+    args = [
+      { title: title, description: description },
+      { type: 'custom', text: html }
+    ]
+
+    args.to_json
+  end
 end
